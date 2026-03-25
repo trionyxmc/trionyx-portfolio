3db8ca170205735c6d830e635c9fdad7c003c0c5
trionyxmc <trionyxmc@gmail.com>
2026-03-25 13:10:28 -0300

    remove pnpm lockfile

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     unoptimized: true,
   },
-  basePath: '/trionyx-portfolio',
-  assetPrefix: '/trionyx-portfolio/',
 }
 
 export default nextConfig
